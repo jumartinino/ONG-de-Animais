346ff68945fbc9a6bcffabfdf519ba018e3ff4f1
Julia <jumartinino@gmail.com>
2022-06-12 16:20:50 -0300

    feature:funcionamento da navbar

---
+++ src/pages/Sobre/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+
+export default function Sobre() {
+    return(
+        <body>
+            
+                
+                <text>Sobre nós</text>
+                
+
+
+
+        </body>
+        
+        
+    )
+}
